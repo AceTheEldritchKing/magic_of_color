c3907316295cd567933415968eb2edbf4205b099
Ace The King <solcat0414@gmail.com>
2025-11-09 01:49:59 -0500

    Changelog:
    Working on recipes now

--- src/main/java/net/acetheeldritchking/magic_of_color/registries/ItemRegistries.java
+++ src/main/java/net/acetheeldritchking/magic_of_color/registries/ItemRegistries.java
@@ -37,6 +37,11 @@ public class ItemRegistries {
     public static final DeferredHolder<Item, Item> BEDROCK_WARLOCK_LEGGINGS = ITEMS.register("bedrock_warlock_leggings", () -> new BedrockWarlockArmorItem(ArmorItem.Type.LEGGINGS, ItemPropertiesHelper.equipment(1).fireResistant().durability(ArmorItem.Type.LEGGINGS.getDurability(40)).component(DataComponents.UNBREAKABLE, new Unbreakable(false))));
     public static final DeferredHolder<Item, Item> BEDROCK_WARLOCK_GREAVES = ITEMS.register("bedrock_warlock_greaves", () -> new BedrockWarlockArmorItem(ArmorItem.Type.BOOTS, ItemPropertiesHelper.equipment(1).fireResistant().durability(ArmorItem.Type.BOOTS.getDurability(40)).component(DataComponents.UNBREAKABLE, new Unbreakable(false))));
 
+    // Gilded Magician's armors
+    // Gilded Magician's Hat
+    // Gilded Magician's Hood
+    // Gilded Magician's Mask
+
     public static Collection<DeferredHolder<Item, ? extends Item>> getDTEItems()
     {
         return ITEMS.getEntries();
